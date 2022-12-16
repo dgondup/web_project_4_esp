0b8e0415d2b5bc4bad68bcbf5e332ec05128c3f7
dgondup <dgondup@gmail.com>
2022-12-16 00:47:00 -0800

    intento

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 let formElement = document.querySelector('.popup');
 let buttonEdit = document.querySelector('.profile__button-edit');
 let buttonClose = document.querySelector('.popup__close-button');
+let buttonSave = formElement.querySelector('popup__button');
 
 function openPopup() {
   formElement.classList.add('popup__opened');
@@ -15,10 +16,20 @@ function closePopup() {
 buttonClose.addEventListener('click', closePopup);
 
 function handleProfileFormSubmit(evt) {
-    evt.preventDefault();
-    let nameInput = formElement.querySelector('#field1');
-    let jobInput = formElement.querySelector('#field2');
-    nameInput.getAttribute('value');
-    jobInput.getAttribute('value');
+  evt.preventDefault();
+  let nameInput = formElement.querySelector('#field1');
+  nameInput.getAttribute('value') = value;
 
+  let jobInput = formElement.querySelector('#field2');
+
+    // Obtén los valores de cada campo desde la propiedad de valor correspondiente
+
+    // Selecciona los elementos donde se introducirán los valores de los campos
+
+    // Inserta nuevos valores utilizando el textContent
+    // propiedad del método querySelector()
 }
+// Conecta el manipulador (handler) al formulario:
+// se observará el evento de entrega
+
+formElement.addEventListener('submit', handleProfileFormSubmit);
