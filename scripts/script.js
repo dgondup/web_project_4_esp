ce44318ce04be52ecdad11813067d1d25b50069d
dgondup <dgondup@gmail.com>
2022-12-20 00:21:27 -0800

    otro intento

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 let formElement = document.querySelector('.popup');
 let buttonEdit = document.querySelector('.profile__button-edit');
 let buttonClose = document.querySelector('.popup__close-button');
-let buttonSave = formElement.querySelector('popup__button');
+let buttonSave = formElement.querySelector('.popup__button');
+let nameInput = formElement.querySelector('#field1');
+let profileName = document.querySelector('.profile__name');
 
 function openPopup() {
   formElement.classList.add('popup__opened');
@@ -17,8 +19,13 @@ buttonClose.addEventListener('click', closePopup);
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  let nameInput = formElement.querySelector('#field1');
-  nameInput.getAttribute('value') = value;
+  nameInput.addEventListener(function (event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+})
+}
+  //nameInput.getAttribute('value') = value;
 
   //let jobInput = formElement.querySelector('#field2');
-}
+
+  formElement.addEventListener('submit', handleProfileFormSubmit);
